12b6386aa8345fcd9f6b69a88d5b60a15533c7a5
FaroukAjimi <benajimi@gmail.com>
2021-02-17 15:57:25 +0100

    SOo

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pm;
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+            {
+                Debug.Log("log");
+                Pause();
+            }
+        }
+    }
+    public void Resume(){
+        pm.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Pause(){
+        pm.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
