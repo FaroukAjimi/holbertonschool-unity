dd3bdbbf63b44c3ecb67ed232ae1d1dbd9d6e2ac
FaroukAjimi <benajimi@gmail.com>
2021-04-07 23:48:10 +0100

    Soo

---
+++ 0x09-unity_ar_business_card/Assets/scripts/redirect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class redirect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string url;
+    public void Open()
+    {
+        Application.OpenURL(url);
+    }
+}
